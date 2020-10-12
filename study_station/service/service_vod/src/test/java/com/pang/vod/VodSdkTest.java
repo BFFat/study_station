2aa9164b78a0f2dfe0157ed3e588a978c8df1ec2
BFFat <37758581+BFFat@users.noreply.github.com>
2020-10-12 14:48:02 +0800

    Update VodSdkTest.java

--- study_station/service/service_vod/src/test/java/com/pang/vod/VodSdkTest.java
+++ study_station/service/service_vod/src/test/java/com/pang/vod/VodSdkTest.java
@@ -1,6 +1,6 @@
 package com.pang.vod;
 
 public class VodSdkTest {
-    String accessKeyId = "LTAI4GBsFihC52qz927yzute";
-    String accessKeySecret = "MDuFxYrN5UcPOjbqerR65xijfe2tHY";
+    String accessKeyId = "";
+    String accessKeySecret = "";
 }
